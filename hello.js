98fbeb98d61aa4f9882cf39d40033b63b8d5a4d8
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-27 08:49:17 +0100

    My JScript Adventure

---
+++ hello.js
@@ -0,0 +1,17 @@
+console.log("Hello World Learning JS")
+
+var firstName = "Magnifico";
+var age = 21;
+var isMale = true;
+var balance = 50000.50;
+var dob = new Date(1996, 3, 2);
+var person = {};
+var empty = undefined;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMale);
+console.log(typeof balance);
+console.log(typeof dob);
+console.log(typeof person);
+console.log(typeof empty);
