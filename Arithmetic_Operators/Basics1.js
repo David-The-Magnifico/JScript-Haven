a4abb97f1fd9c4dae85d6aff11c6774dc7209740
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:28:29 +0100

    JScript Basics

---
+++ Arithmetic_Operators/Basics1.js
@@ -0,0 +1,13 @@
+// Arithmetic Operators
+var addition = 3 + 5;
+var subtraction = 3 - 5;
+var multiplication = 3 * (2 + 4) * 5;
+var division = 3 / 5;
+var remainder = 10 % 4;
+var exponentation = 10 ** 4;
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(remainder);
+console.log(exponentation);
