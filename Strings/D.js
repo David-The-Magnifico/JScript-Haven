839d48c1081081dfa8fd91aaff0807f26ef867ff
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:38:41 +0100

    JScript Basics

---
+++ Strings/D.js
@@ -0,0 +1,28 @@
+// Strings
+var brand = "The Magnifico"
+console.log(typeof brand)
+console.log(brand.length)
+console.log(brand.toUpperCase())
+console.log(brand.substring(4, 13))
+console.log(brand.charAt(0))
+console.log(brand.charAt(1))
+console.log(brand.charAt(2))
+console.log(brand.charAt(3))
+console.log(brand.charAt(4))
+console.log(brand.charAt(5))
+console.log(brand.charAt(6))
+console.log(brand.charAt(7))
+console.log(brand.charAt(8))
+console.log(brand.charAt(9))
+console.log(brand.charAt(10))
+console.log(brand.charAt(11))
+console.log(brand.charAt(12))
+console.log(brand.charAt(13))
+console.log(brand.charAt(14))
+
+var a = "Bless"
+var b = "ing"
+var c = "Ben"
+var d = "jamin"
+console.log(a+b+ " " + c+d)
+console.log(`${a}${b} ${c.toUpperCase()}${d.toUpperCase()}`)
