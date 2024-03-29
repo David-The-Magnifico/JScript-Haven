c61b142bc38eeb2a8cc1e4bbc86ddad943e3f939
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:39:20 +0100

    JScript Basics

---
+++ Strings/Tinker.js
@@ -0,0 +1,31 @@
+
+var person = {
+    id: "ISBN 134587243",
+    available: true,
+    count: 7,
+    name: "The Conquest of the Void Emperor",
+    author: "The Magnifico",
+    price: "1000 USD",
+    description: "The Conquest of the Void Emperor is a 2000-year-old novel written by The Magnifico. The Story is about a young man who was promoted to the Void Tower after having a mysterious encounter which brought him good fortune and fame. Let us join him on this adventure!!!.",
+    image: "https://images-na.ssl-images-amazon.com/images/I/51-9-1%2B%2B%2B%2B%2B%2B%2B.jpg",
+    genre: "Fantasy",
+    publisher: "Magnifico Books",
+    publishDate: new Date(2018, 6, 17),
+    pages: 1000,
+    rating: 4.5,
+}
+
+console.log(person);
+console.log(typeof person.id);
+console.log(typeof person.available);
+console.log(typeof person.count);
+console.log(typeof person.name);
+console.log(typeof person.author);
+console.log(typeof person.price);
+console.log(typeof person.description);
+console.log(typeof person.image);
+console.log(typeof person.genre);
+console.log(typeof person.publisher);
+console.log(typeof person.publishDate);
+console.log(typeof person.pages);
+console.log(typeof person.rating);
