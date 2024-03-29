6f1ef30f2d1bb8ad591961b9fff8624ec3fcf2dc
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:38:06 +0100

    JScript Basics

---
+++ Boolean/Basics3.js
@@ -0,0 +1,12 @@
+// Boolean
+
+var isAdult = true
+
+if (isAdult) {
+    console.log("You are an adult")
+} else {
+    console.log("You are not an adult")
+}
+
+console.log(!true)
+console.log(!false)
