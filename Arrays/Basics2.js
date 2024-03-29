dc9c138cb45998b1013ca7b303a6a4d0f2ece67b
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:32:24 +0100

    JScript Basics

---
+++ Arrays/Basics2.js
@@ -0,0 +1,18 @@
+// Arrays
+var names = [
+    "Magnifico",
+    "Osmond",
+    "O'Bryan",
+    "Vladimir",
+    "Ronaldo",
+    "Blessing"
+];
+console.log(names);
+console.log("Index 0 - " + names[0]);
+console.log("Index 1 - " + names[1]);
+console.log("Index 2 - " + names[2]);
+console.log("Index 3 - " + names[3]);
+console.log("Index 4 - " + names[4]);
+console.log("Index 5 - " + names[5]);
+console.log("Index 6 - " + names[6]);
+console.log("Size = " + names.length);
