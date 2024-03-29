585de2a9d3591d837cd7c1dd07d3a11ce5333f8a
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:38:30 +0100

    JScript Basics

---
+++ Loops/Basics5.js
@@ -0,0 +1,27 @@
+// Loops
+var names = [
+    "Magnifico",
+    "Osmond",
+    "O'Bryan",
+    "Vladimir",
+    "Ronaldo",
+    "Blessing",
+    "Abbey"
+];
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+console.log();
+console.log("for of");
+
+for (const name of names) {
+    console.log(name);
+}
+
+console.log();
+console.log("forEach");
+names.forEach(function(name) {
+    console.log(name)
+})
