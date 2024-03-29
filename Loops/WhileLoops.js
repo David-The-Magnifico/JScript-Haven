63309dc39073262a2b0310191f3db5e6bf4c38ec
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:39:29 +0100

    JScript Basics

---
+++ Loops/WhileLoops.js
@@ -0,0 +1,16 @@
+// While loops
+var names = [
+    "Magnifico",
+    "Osmond",
+    "O'Bryan",
+    "Vladimir",
+    "Ronaldo",
+    "Blessing"
+];
+
+var index = 0;
+
+while(index < names.length) {
+    console.log(names[index]);
+    index = index  + 1;
+}
