1ff3cf71e6243cb3a5ed7e195391d06d255173ac
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-29 07:38:51 +0100

    JScript Basics

--- hello.js
+++ hello.js
@@ -1,17 +1,36 @@
 console.log("Hello World Learning JS")
 
-var firstName = "Magnifico";
-var age = 21;
-var isMale = true;
-var balance = 50000.50;
-var dob = new Date(1996, 3, 2);
-var person = {};
-var empty = undefined;
+var person = {
+    firstName: "Magnifico",
+    age: 2,
+    isMale: true,
+    balance: 50000.50,
+    dob: new Date(1996, 3, 2).toJSON(),
+    address: {
+        city: "Lagos",
+        postalCode: "100001",
 
-console.log(typeof firstName);
-console.log(typeof age);
-console.log(typeof isMale);
-console.log(typeof balance);
-console.log(typeof dob);
-console.log(typeof person);
-console.log(typeof empty);
+    }
+};
+
+console.log(person.firstName);
+console.log(person.age);
+console.log(person.isMale);
+console.log(person.balance);
+console.log(person.dob);
+console.log(person.address.city);
+console.log(Object.values(person));
+console.log(Object.keys(person));
+console.log(Object.entries(person));
+console.log(Object.getOwnPropertyNames(person));
+console.log(Object.getOwnPropertySymbols(person));
+console.log(Object.getOwnPropertyDescriptor(person, "firstName"));
+console.log();
+console.log();
+
+console.log(typeof person.firstName);
+console.log(typeof person.age);
+console.log(typeof person.isMale);
+console.log(typeof person.balance);
+console.log(typeof person.dob);
+console.log(typeof person.address.city);
