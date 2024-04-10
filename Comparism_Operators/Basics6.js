b170b6a3ae07ae9872318a2cea96647d8da32f15
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-10 06:32:19 +0100

    JScript Basics

---
+++ Comparism_Operators/Basics6.js
@@ -0,0 +1,9 @@
+console.log(10 == 10)
+console.log(10 != 10)
+console.log(10 > 10)
+console.log(10 < 10)
+console.log(10 >= 10)
+console.log(10 <= 10)
+
+console.log("A" == "B")
+console.log("A" != "B")
