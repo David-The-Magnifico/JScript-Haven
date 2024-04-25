9399565a375b0886933b6159fd02f56dbc773420
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-04-25 10:55:34 +0100

    JS OOP:Objects

--- objects.js
+++ objects.js
@@ -17,5 +17,14 @@ Object.setPrototypeOf(goat, animal);
 goat.type = "Goat";
 console.log(goat);
 
-//dog.sound();
-//console.log(dog)
+let vehicle = {wheel: 4};
+let car = {
+    seats: 5,
+    _proto_: vehicle,
+    wheels: 6,
+
+};
+
+console.log('vehicle:', vehicle, vehicle._proto_);
+console.log('car:', car, car._proto_);
+console.log('car.wheel:', car.wheel);
