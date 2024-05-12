09cad9c7207ad03ecd0777be4c10a34067c53c15
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-05-13 00:28:45 +0100

    practice

--- OOP/functionParam.js
+++ OOP/functionParam.js
@@ -8,3 +8,18 @@ function Person(name, age) {
 }
 const PersonOne = new Person('Blessing', 25);
 console.log(PersonOne.name);
+
+
+function Human(_name, _age) {
+    this.name = _name;
+    this.age = _age;
+}
+
+function Eployee(_name, _age, _designation){
+    this.designation = _designation;
+    this.setAge = newage => {
+        console.log('setting age from ${this.age} to ${newage}')
+    }
+}
+    }
+}
